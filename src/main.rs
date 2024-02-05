84120e852476df690ea497ada445b29c91280aa0
Jason Dagit <dagitj@gmail.com>
2024-02-04 20:58:31 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,419 @@
+pub mod usb2snes;
+
+use lazy_static::lazy_static;
+use std::collections::BTreeMap;
+use usb2snes::*;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Samus {
+    hp: u16,
+    max_hp: u16,
+    missiles: u16,
+    max_missiles: u16,
+    supers: u16,
+    max_supers: u16,
+    pbs: u16,
+    max_pbs: u16,
+    equipped_items: Vec<Item>,
+    collected_items: Vec<Item>,
+    equipped_beams: Vec<Beam>,
+    collected_beams: Vec<Beam>,
+    reserve_hp: u16,
+    max_reserve_hp: u16,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum SamusField {
+    HP,
+    MaxHP,
+    Missiles,
+    MaxMissiles,
+    Supers,
+    MaxSupers,
+    PBs,
+    MaxPBs,
+    EquippedItems,
+    CollectedItems,
+    EquippedBeams,
+    CollectedBeams,
+    ReserveHP,
+    MaxReserveHP,
+}
+
+lazy_static! {
+    static ref SAMUS_ADDR_MAP: BTreeMap<SamusField, u16> = {
+        let mut m = BTreeMap::new();
+        m.insert(SamusField::HP, 0x09C2);
+        m.insert(SamusField::MaxHP, 0x09C4);
+        m.insert(SamusField::Missiles, 0x09C6);
+        m.insert(SamusField::MaxMissiles, 0x09C8);
+        m.insert(SamusField::Supers, 0x09CA);
+        m.insert(SamusField::MaxSupers, 0x09CC);
+        m.insert(SamusField::PBs, 0x09CE);
+        m.insert(SamusField::MaxPBs, 0x09D0);
+        m.insert(SamusField::EquippedItems, 0x09A2);
+        m.insert(SamusField::CollectedItems, 0x09A4);
+        m.insert(SamusField::EquippedBeams, 0x09A6);
+        m.insert(SamusField::CollectedBeams, 0x09A8);
+        m.insert(SamusField::ReserveHP, 0x09D6);
+        m.insert(SamusField::MaxReserveHP, 0x09D4);
+        m
+    };
+}
+
+const WRAM: u32 = 0xF5_0000;
+
+const VARIA: u16 = 1;
+const SPRINGBALL: u16 = 2;
+const MORPHBALL: u16 = 4;
+const SCREWATTACK: u16 = 8;
+const GRAVITY: u16 = 0x20;
+const HIJUMPBOOTS: u16 = 0x100;
+const SPACEJUMP: u16 = 0x200;
+const BOMBS: u16 = 0x1000;
+const SPEEDBOOSTER: u16 = 0x2000;
+const GRAPPLE: u16 = 0x4000;
+const XRAY: u16 = 0x8000;
+const WAVE: u16 = 1;
+const ICE: u16 = 2;
+const SPAZER: u16 = 4;
+const PLASMA: u16 = 8;
+const CHARGE: u16 = 0x1000;
+
+const CMD_SPACE: u32 = 0x2c00;
+
+#[repr(u16)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum Item {
+    Varia = VARIA,
+    SpringBall = SPRINGBALL,
+    MorphBall = MORPHBALL,
+    ScrewAttack = SCREWATTACK,
+    Gravity = GRAVITY,
+    HiJumpBoots = HIJUMPBOOTS,
+    SpaceJump = SPACEJUMP,
+    Bombs = BOMBS,
+    SpeedBooster = SPEEDBOOSTER,
+    Grapple = GRAPPLE,
+    XRay = XRAY,
+}
+
+#[repr(u16)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum Beam {
+    Wave = WAVE,
+    Ice = ICE,
+    Spazer = SPAZER,
+    Plasma = PLASMA,
+    Charge = CHARGE,
+}
+
+fn items_to_u16(items: &[Item]) -> u16 {
+    let mut r = 0u16;
+    for i in items {
+        r = r.wrapping_add(*i as u16);
+    }
+    r
+}
+
+fn u16_to_items(items: u16) -> Vec<Item> {
+    let mut r = Vec::new();
+    if items & VARIA == VARIA {
+        r.push(Item::Varia);
+    }
+    if items & SPRINGBALL == SPRINGBALL {
+        r.push(Item::SpringBall);
+    }
+    if items & MORPHBALL == MORPHBALL {
+        r.push(Item::MorphBall);
+    }
+    if items & SCREWATTACK == SCREWATTACK {
+        r.push(Item::ScrewAttack);
+    }
+    if items & GRAVITY == GRAVITY {
+        r.push(Item::Gravity);
+    }
+    if items & HIJUMPBOOTS == HIJUMPBOOTS {
+        r.push(Item::HiJumpBoots);
+    }
+    if items & SPACEJUMP == SPACEJUMP {
+        r.push(Item::SpaceJump);
+    }
+    if items & BOMBS == BOMBS {
+        r.push(Item::Bombs);
+    }
+    if items & SPEEDBOOSTER == SPEEDBOOSTER {
+        r.push(Item::SpeedBooster);
+    }
+    if items & GRAPPLE == GRAPPLE {
+        r.push(Item::Grapple);
+    }
+    if items & XRAY == XRAY {
+        r.push(Item::XRay);
+    }
+
+    r
+}
+
+fn beams_to_u16(beams: &[Beam]) -> u16 {
+    let mut r = 0u16;
+    for b in beams {
+        r = r.wrapping_add(*b as u16);
+    }
+    r
+}
+
+fn u16_to_beams(beams: u16) -> Vec<Beam> {
+    let mut r = Vec::new();
+    if beams & WAVE == WAVE {
+        r.push(Beam::Wave);
+    }
+    if beams & ICE == ICE {
+        r.push(Beam::Ice);
+    }
+    if beams & SPAZER == SPAZER {
+        r.push(Beam::Spazer);
+    }
+    if beams & PLASMA == PLASMA {
+        r.push(Beam::Plasma);
+    }
+    if beams & CHARGE == CHARGE {
+        r.push(Beam::Charge);
+    }
+
+    r
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let mut client = crate::usb2snes::SyncClient::connect()?;
+    client.set_name("goofgenie")?;
+    let device_list = client.list_device()?.to_vec();
+    println!("{:#?}", device_list);
+    if device_list.len() == 1 {
+        client.attach(&device_list[0])?;
+    }
+
+    println!("{:#?}", client.info());
+
+    // preamble corresponds to:
+    // php
+    // rep #$30
+    // pha
+    // phx
+    // phy
+    // phb
+    //
+    // And postamble corresponds to:
+    // plb
+    // stz $2c00 ; disable this command
+    // ply
+    // plx
+    // pla
+    // plp
+    // jmp ($ffea) ; run the normal nmi code
+    //
+    let preamble: [u8; 7] = [0x08, 0xc2, 0x30, 0x48, 0xda, 0x5a, 0x8b];
+    let postamble: [u8; 11] = [
+        0xab, 0x9c, 0x00, 0x2c, 0x7a, 0xfa, 0x68, 0x28, 0x6c, 0xea, 0xff,
+    ];
+    let samus = get_samus(&mut client)?;
+    // Example samus edit:
+    //samus.collected_items.push(Item::MorphBall);
+    //samus.collected_items.push(Item::Bombs);
+    //samus.collected_items.push(Item::XRay);
+    //samus.equipped_items.push(Item::XRay);
+    //samus.collected_items.push(Item::SpaceJump);
+    //samus.collected_beams.push(Beam::Wave);
+    //samus.equipped_beams.push(Beam::Wave);
+    //samus.hp = 50;
+    //samus.max_reserve_hp = 100;
+    //samus.reserve_hp = 10;
+    //samus.missiles = 10;
+    //samus.max_missiles = 100;
+    //samus.supers = 100;
+    //samus.max_supers = 100;
+    let mut data = Vec::new();
+    data.extend_from_slice(&preamble);
+    data.extend_from_slice(&samus_overwrite_asm(&samus));
+    //data.extend_from_slice(&spike_suit_asm());
+    //data.extend_from_slice(&blue_suit_asm());
+    //data.extend_from_slice(&g_mode_asm());
+    data.extend_from_slice(&postamble);
+    assert!(data.len() <= 512);
+    client.put_cmd_address(CMD_SPACE, &data)?;
+
+    let samus = get_samus(&mut client)?;
+    println!("{:#?}", samus);
+    //println!("{:02x?}", data);
+
+    Ok(())
+}
+
+fn lda_immediate_u16(data: u16) -> [u8; 3] {
+    let bytes = u16_to_le(data);
+    [0xa9, bytes[0], bytes[1]]
+}
+
+fn lda_immediate_u8(data: u8) -> [u8; 2] {
+    [0xa9, data]
+}
+
+fn sta_u16(data: u16) -> [u8; 3] {
+    let bytes = u16_to_le(data);
+    [0x8d, bytes[0], bytes[1]]
+}
+
+fn get_u16(
+    client: &mut SyncClient,
+    address: u32,
+) -> std::result::Result<u16, Box<dyn std::error::Error>> {
+    let response = client.get_address(address, 2)?;
+    Ok(((response[1] as u16) << 8) + response[0] as u16)
+}
+
+fn get_wram_addr(field: SamusField) -> u32 {
+    *SAMUS_ADDR_MAP.get(&field).unwrap() as u32 + WRAM
+}
+
+fn get_samus(client: &mut SyncClient) -> std::result::Result<Samus, Box<dyn std::error::Error>> {
+    let hp = get_u16(client, get_wram_addr(SamusField::HP))?;
+    let max_hp = get_u16(client, get_wram_addr(SamusField::MaxHP))?;
+    let missiles = get_u16(client, get_wram_addr(SamusField::Missiles))?;
+    let max_missiles = get_u16(client, get_wram_addr(SamusField::MaxMissiles))?;
+    let supers = get_u16(client, get_wram_addr(SamusField::Supers))?;
+    let max_supers = get_u16(client, get_wram_addr(SamusField::MaxSupers))?;
+    let pbs = get_u16(client, get_wram_addr(SamusField::PBs))?;
+    let max_pbs = get_u16(client, get_wram_addr(SamusField::MaxPBs))?;
+    let equipped_items = u16_to_items(get_u16(client, get_wram_addr(SamusField::EquippedItems))?);
+    let collected_items = u16_to_items(get_u16(client, get_wram_addr(SamusField::CollectedItems))?);
+    let equipped_beams = u16_to_beams(get_u16(client, get_wram_addr(SamusField::EquippedBeams))?);
+    let collected_beams = u16_to_beams(get_u16(client, get_wram_addr(SamusField::CollectedBeams))?);
+    let reserve_hp = get_u16(client, get_wram_addr(SamusField::ReserveHP))?;
+    let max_reserve_hp = get_u16(client, get_wram_addr(SamusField::MaxReserveHP))?;
+
+    Ok(Samus {
+        hp,
+        max_hp,
+        missiles,
+        max_missiles,
+        supers,
+        max_supers,
+        pbs,
+        max_pbs,
+        equipped_items,
+        collected_items,
+        reserve_hp,
+        max_reserve_hp,
+        collected_beams,
+        equipped_beams,
+    })
+}
+
+pub fn u16_to_le(data: u16) -> [u8; 2] {
+    let lb: u8 = (data & 0x00FF) as u8;
+    let hb: u8 = (data >> 8) as u8;
+    assert_eq!(data, ((hb as u16) << 8) + lb as u16);
+    [lb, hb]
+}
+
+pub fn samus_overwrite_asm(samus: &Samus) -> Vec<u8> {
+    let mut r = Vec::new();
+    // First write samus's collected items
+    r.extend_from_slice(&lda_immediate_u16(items_to_u16(&samus.collected_items)));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::CollectedItems).unwrap(),
+    ));
+    // Next we do equipped items
+    r.extend_from_slice(&lda_immediate_u16(items_to_u16(&samus.equipped_items)));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::EquippedItems).unwrap(),
+    ));
+    // Next we do collected beams
+    r.extend_from_slice(&lda_immediate_u16(beams_to_u16(&samus.collected_beams)));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::CollectedBeams).unwrap(),
+    ));
+    // Next we do equipped beams
+    r.extend_from_slice(&lda_immediate_u16(beams_to_u16(&samus.equipped_beams)));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::EquippedBeams).unwrap(),
+    ));
+    // Next we do HP
+    r.extend_from_slice(&lda_immediate_u16(samus.hp));
+    r.extend_from_slice(&sta_u16(*SAMUS_ADDR_MAP.get(&SamusField::HP).unwrap()));
+    // Next we do MaxHP
+    r.extend_from_slice(&lda_immediate_u16(samus.max_hp));
+    r.extend_from_slice(&sta_u16(*SAMUS_ADDR_MAP.get(&SamusField::MaxHP).unwrap()));
+    // Next we do Missiles
+    r.extend_from_slice(&lda_immediate_u16(samus.missiles));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::Missiles).unwrap(),
+    ));
+    // Next we do MaxMissiles
+    r.extend_from_slice(&lda_immediate_u16(samus.max_missiles));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::MaxMissiles).unwrap(),
+    ));
+    // Next we do Supers
+    r.extend_from_slice(&lda_immediate_u16(samus.supers));
+    r.extend_from_slice(&sta_u16(*SAMUS_ADDR_MAP.get(&SamusField::Supers).unwrap()));
+    // Next we do MaxSupers
+    r.extend_from_slice(&lda_immediate_u16(samus.max_supers));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::MaxSupers).unwrap(),
+    ));
+    // Next we do Power Bombs
+    r.extend_from_slice(&lda_immediate_u16(samus.pbs));
+    r.extend_from_slice(&sta_u16(*SAMUS_ADDR_MAP.get(&SamusField::PBs).unwrap()));
+    // Next we do MaxPBs
+    r.extend_from_slice(&lda_immediate_u16(samus.max_pbs));
+    r.extend_from_slice(&sta_u16(*SAMUS_ADDR_MAP.get(&SamusField::MaxPBs).unwrap()));
+    // Next we do Reserves
+    r.extend_from_slice(&lda_immediate_u16(samus.reserve_hp));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::ReserveHP).unwrap(),
+    ));
+    // Next we do MaxPBs
+    r.extend_from_slice(&lda_immediate_u16(samus.max_reserve_hp));
+    r.extend_from_slice(&sta_u16(
+        *SAMUS_ADDR_MAP.get(&SamusField::MaxReserveHP).unwrap(),
+    ));
+
+    r
+}
+
+pub fn blue_suit_asm() -> Vec<u8> {
+    let mut r = Vec::new();
+    // sep #$20
+    r.push(0xe2);
+    r.push(0x20);
+    r.extend_from_slice(&lda_immediate_u8(4));
+    r.extend_from_slice(&sta_u16(0x0b3f));
+    // rep #$20
+    r.push(0xc2);
+    r.push(0x20);
+
+    r
+}
+
+pub fn spike_suit_asm() -> Vec<u8> {
+    let mut r = Vec::new();
+    // sep #$20
+    r.push(0xe2);
+    r.push(0x20);
+    r.extend_from_slice(&lda_immediate_u8(1));
+    r.extend_from_slice(&sta_u16(0x0a68));
+    // rep #$20
+    r.push(0xc2);
+    r.push(0x20);
+
+    r
+}
+
+pub fn g_mode_asm() -> Vec<u8> {
+    let mut r = Vec::new();
+    r.extend_from_slice(&lda_immediate_u16(0x0000));
+    r.extend_from_slice(&sta_u16(0x1c23));
+
+    r
+}
---
+++ src/usb2snes.rs
@@ -0,0 +1,357 @@
+/*
+ * Copyright (c) 2021 Sylvain "Skarsnik" Colinet
+ *
+ * This file is part of the usb2snes-cli project.
+ * (see https://github.com/usb2snes/usb2snes-cli).
+ *
+ * usb2snes-cli is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * usb2snes-cli is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QUsb2Snes.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#![allow(dead_code)]
+
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::net::TcpStream;
+use strum_macros::Display;
+use tungstenite::protocol::WebSocket;
+use tungstenite::stream::MaybeTlsStream;
+use tungstenite::Message;
+
+use std::borrow::Cow;
+use std::rc::Rc;
+
+#[derive(Display, Debug)]
+#[allow(dead_code)]
+pub enum Command {
+    AppVersion,
+    Name,
+    DeviceList,
+    Attach,
+    Info,
+    Boot,
+    Reset,
+    Menu,
+
+    List,
+    PutFile,
+    GetFile,
+    Rename,
+    Remove,
+
+    GetAddress,
+    PutAddress,
+}
+#[derive(Display, Debug)]
+#[allow(dead_code)]
+pub enum Space {
+    None,
+    SNES,
+    CMD,
+}
+
+#[derive(Debug)]
+pub struct Infos {
+    pub version: Rc<str>,
+    pub dev_type: Rc<str>,
+    pub game: Rc<str>,
+    pub flags: Vec<Rc<str>>,
+}
+
+#[derive(Serialize)]
+#[allow(non_snake_case)]
+struct USB2SnesQuery<'a> {
+    Opcode: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    Space: Option<String>,
+    Flags: Vec<String>,
+    Operands: &'a [Cow<'a, str>],
+}
+
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct USB2SnesResult {
+    Results: Rc<[Rc<str>]>,
+}
+
+#[derive(Eq, PartialEq)]
+pub enum USB2SnesFileType {
+    File = 0,
+    Dir = 1,
+}
+
+pub struct USB2SnesFileInfo {
+    pub name: Rc<str>,
+    pub file_type: USB2SnesFileType,
+}
+
+pub struct SyncClient {
+    client: WebSocket<MaybeTlsStream<TcpStream>>,
+    devel: bool,
+}
+
+impl SyncClient {
+    pub fn connect() -> Result<SyncClient, Box<dyn Error>> {
+        Ok(SyncClient {
+            client: tungstenite::client::connect("ws://localhost:23074")?.0,
+            devel: false,
+        })
+    }
+
+    pub fn connect_with_devel() -> Result<SyncClient, Box<dyn Error>> {
+        Ok(SyncClient {
+            client: tungstenite::client::connect("ws://localhost:23074")?.0,
+            devel: true,
+        })
+    }
+
+    fn send_command(&mut self, command: Command, args: &[Cow<str>]) -> Result<(), Box<dyn Error>> {
+        self.send_command_with_space(command, None, args)
+    }
+
+    fn send_command_with_space(
+        &mut self,
+        command: Command,
+        space: Option<Space>,
+        args: &[Cow<str>],
+    ) -> Result<(), Box<dyn Error>> {
+        if self.devel {
+            println!("Send command : {:?}", command);
+        }
+        let nspace = space.map(|sp| sp.to_string());
+        let query = USB2SnesQuery {
+            Opcode: command.to_string(),
+            Space: nspace,
+            Flags: vec![],
+            Operands: args,
+        };
+        let json = serde_json::to_string(&query)?;
+        if self.devel {
+            let json = serde_json::to_string_pretty(&query)?;
+            println!("{}", json);
+        }
+        let message = Message::text(json);
+        Ok(self.client.send(message)?)
+    }
+
+    fn get_reply(&mut self) -> Result<USB2SnesResult, Box<dyn Error>> {
+        let reply = self.client.read()?;
+        let mut textreply: String = String::from("");
+        match reply {
+            Message::Text(value) => {
+                textreply = value;
+            }
+            _ => Err("Error getting a reply")?,
+        };
+        if self.devel {
+            println!("Reply:");
+            println!("{}", textreply);
+        }
+        Ok(serde_json::from_str(&textreply)?)
+    }
+
+    pub fn set_name(&mut self, name: &str) -> Result<(), Box<dyn Error>> {
+        self.send_command(Command::Name, &[Cow::Borrowed(name)])
+    }
+
+    pub fn app_version(&mut self) -> Result<String, Box<dyn Error>> {
+        self.send_command(Command::AppVersion, &[])?;
+        let usbreply = self.get_reply()?;
+        Ok(usbreply.Results[0].to_string())
+    }
+
+    pub fn list_device(&mut self) -> Result<Rc<[Rc<str>]>, Box<dyn Error>> {
+        self.send_command(Command::DeviceList, &[])?;
+        let usbreply = self.get_reply()?;
+        Ok(usbreply.Results)
+    }
+
+    pub fn attach(&mut self, device: &str) -> Result<(), Box<dyn Error>> {
+        self.send_command(Command::Attach, &[Cow::Borrowed(device)])
+    }
+
+    pub fn info(&mut self) -> Result<Infos, Box<dyn Error>> {
+        self.send_command(Command::Info, &[])?;
+        let usbreply = self.get_reply()?;
+        let info = usbreply.Results;
+        Ok(Infos {
+            version: info[0].clone(),
+            dev_type: info[1].clone(),
+            game: info[2].clone(),
+            flags: info[3..].to_owned(),
+        })
+    }
+
+    pub fn reset(&mut self) -> Result<(), Box<dyn Error>> {
+        self.send_command(Command::Reset, &[])
+    }
+
+    pub fn menu(&mut self) -> Result<(), Box<dyn Error>> {
+        self.send_command(Command::Menu, &[])
+    }
+
+    pub fn boot(&mut self, toboot: &str) -> Result<(), Box<dyn Error>> {
+        self.send_command(Command::Boot, &[Cow::Borrowed(toboot)])
+    }
+
+    pub fn ls(&mut self, path: &str) -> Result<Vec<USB2SnesFileInfo>, Box<dyn Error>> {
+        self.send_command(Command::List, &[Cow::Borrowed(path)])?;
+        let usbreply = self.get_reply()?;
+        let vec_info = usbreply.Results;
+        let mut toret: Vec<USB2SnesFileInfo> = vec![];
+        let mut i = 0;
+        while i < vec_info.len() {
+            let info: USB2SnesFileInfo = USB2SnesFileInfo {
+                file_type: if &*vec_info[i] == "1" {
+                    USB2SnesFileType::File
+                } else {
+                    USB2SnesFileType::Dir
+                },
+                name: vec_info[i + 1].clone(),
+            };
+            toret.push(info);
+            i += 2;
+        }
+        Ok(toret)
+    }
+
+    pub fn send_file(&mut self, path: &str, data: &[u8]) -> Result<(), Box<dyn Error>> {
+        self.send_command(
+            Command::PutFile,
+            &[Cow::Borrowed(path), Cow::Owned(format!("{:x}", data.len()))],
+        )?;
+        let mut start = 0;
+        let mut stop = 1024;
+        while start < data.len() {
+            self.client.send(Message::binary(&data[start..stop]))?;
+            start += 1024;
+            stop += 1024;
+            if stop > data.len() {
+                stop = data.len();
+            }
+        }
+        Ok(())
+    }
+
+    pub fn get_file(&mut self, path: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+        self.send_command(Command::GetFile, &[Cow::Borrowed(path)])?;
+        let string_hex = self.get_reply()?.Results[0].to_string();
+        let size = usize::from_str_radix(&string_hex, 16)?;
+        let mut data: Vec<u8> = Vec::with_capacity(size);
+        loop {
+            let reply = self.client.read()?;
+            match reply {
+                Message::Binary(msgdata) => {
+                    data.extend(&msgdata);
+                }
+                _ => Err("Error getting a reply")?,
+            }
+            if data.len() == size {
+                break;
+            }
+        }
+        Ok(data)
+    }
+
+    pub fn remove_path(&mut self, path: &str) -> Result<(), Box<dyn Error>> {
+        self.send_command(Command::Remove, &[Cow::Borrowed(path)])
+    }
+
+    pub fn get_address(&mut self, address: u32, size: usize) -> Result<Vec<u8>, Box<dyn Error>> {
+        self.send_command_with_space(
+            Command::GetAddress,
+            Some(Space::SNES),
+            &[
+                Cow::Owned(format!("{:x}", address)),
+                Cow::Owned(format!("{:x}", size)),
+            ],
+        )?;
+        let mut data: Vec<u8> = Vec::with_capacity(size);
+        loop {
+            let reply = self.client.read()?;
+            match reply {
+                Message::Binary(msgdata) => {
+                    data.extend(&msgdata);
+                }
+                _ => Err("Error getting a reply")?,
+            }
+            if data.len() == size {
+                break;
+            }
+        }
+        Ok(data)
+    }
+
+    pub fn get_addresses(
+        &mut self,
+        pairs: &[(u32, usize)],
+    ) -> Result<Vec<Vec<u8>>, Box<dyn Error>> {
+        let mut args = Vec::with_capacity(pairs.len() * 2);
+        let mut total_size = 0;
+        for &(address, size) in pairs.iter() {
+            args.push(Cow::Owned(format!("{:x}", address)));
+            args.push(Cow::Owned(format!("{:x}", size)));
+            total_size += size;
+        }
+        self.send_command_with_space(Command::GetAddress, Some(Space::SNES), &args)?;
+        let mut data: Vec<u8> = Vec::with_capacity(total_size);
+        let mut ret: Vec<Vec<u8>> = Vec::with_capacity(pairs.len());
+        loop {
+            let reply = self.client.read()?;
+            match reply {
+                Message::Binary(msgdata) => {
+                    data.extend(&msgdata);
+                }
+                _ => Err("Error getting a reply")?,
+            }
+            if data.len() == total_size {
+                break;
+            }
+        }
+        let mut consumed = 0;
+        for &(_address, size) in pairs.iter() {
+            ret.push(data[consumed..consumed + size].into());
+            consumed += size;
+        }
+        Ok(ret)
+    }
+
+    pub fn put_address(&mut self, address: u32, data: &[u8]) -> Result<(), Box<dyn Error>> {
+        self.send_command_with_space(
+            Command::PutAddress,
+            Some(Space::SNES),
+            &[
+                Cow::Owned(format!("{:x}", address)),
+                Cow::Owned(format!("{:x}", data.len())),
+            ],
+        )?;
+        assert!(data.len() < 1024);
+        self.client.send(Message::binary(data))?;
+        Ok(())
+    }
+
+    // TODO: rename this to something like put_cmd
+    pub fn put_cmd_address(&mut self, address: u32, data: &[u8]) -> Result<(), Box<dyn Error>> {
+        self.send_command_with_space(
+            Command::PutAddress,
+            Some(Space::CMD),
+            &[
+                Cow::Owned(format!("{:x}", address)),
+                Cow::Owned(format!("{:x}", data.len())),
+            ],
+        )?;
+        assert!(data.len() <= 512);
+        self.client.send(Message::binary(data))?;
+        Ok(())
+    }
+}
